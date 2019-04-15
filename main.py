ddafd5b425422b813888cebeffbeddf25ae59269
Irakli Gunia <irakli.gunia@gmail.com>
2019-04-15 16:30:22 +0400

    სატესტო 3

--- main.py
+++ main.py
@@ -21,21 +21,26 @@ class HTTPClient(asyncore.dispatcher):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((host, port))
-        self.buffer = 'GET %s HTTP/1.0\r\n\r\n' % path
+        self.buffer = "test message"
 
     def handle_connect(self):
+    	print("handle_connect")
         pass
 
     def handle_close(self):
+    	print("handle_close")
         self.close()
 
     def handle_read(self):
+    	print("handle_read")
         print self.recv(8192)
 
     def writable(self):
+    	print("writable")
         return (len(self.buffer) > 0)
 
     def handle_write(self):
+    	print("handle_write")
         sent = self.send(self.buffer)
         self.buffer = self.buffer[sent:]
 
