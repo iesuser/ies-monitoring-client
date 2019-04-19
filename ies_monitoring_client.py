0e30062b1ba102f78e73c7bece2ead74fca58917
Irakli Gunia <irakli.gunia@gmail.com>
2019-04-19 19:08:01 +0400

    თითოეულ მესიჯისთვის ეშვება wait_for_server_response thread-ი

--- ies_monitoring_client.py
+++ ies_monitoring_client.py
@@ -17,7 +17,6 @@ server_port = 12345
 message_uuids = {}
 # მესიჯის buffer_size
 buffer_size = 8192
-wait_for_server_response_thread = None
 
 # ფუნქცია ქმნის სოკეტს და უკავშირდება ies_monitoring_server-ს
 def connect_to_ies_monitoring_server():
@@ -32,25 +31,41 @@ def connect_to_ies_monitoring_server():
 def dictionary_to_bytes(dictionary_message):
     return bytes(json.dumps(dictionary_message), 'utf-8')
 
-def wait_for_server_response():
+def wait_for_server_response(connection, message_id):
+    print("=====================wait_for_server_response========================")
     # სანამ message_uuids არ არის ცარიელი
     # სანამ სერვერიდან ველოდებით შეტყობინებას მესიჯის მიღების დასტურად
-    # print(connection)
+    print(id(connection))
     # print(type(connection))
-    while message_uuids:
-        # pass
-        # break
+    while True:
+        # print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+        # print(message_uuids)
+
+        # print(connection)
+        time.sleep(0.2)
         received_message_bytes = connection.recv(buffer_size)
-        message_id = received_message_bytes.decode("utf-8")
-        if message_id in message_uuids:
-            del message_uuids[message_id]
-        if not message_uuids:
+        received_message_id = received_message_bytes.decode("utf-8")
+        if received_message_id == "":
+            print("მესიჯი არ მოსულა")
+            print(datetime.datetime.now() - message_uuids[message_id])
+            continue
+        if received_message_id in message_uuids:
+            print(":::::::" + received_message_id)
+            print(message_uuids)
+            del message_uuids[received_message_id]
+            print("-----------")
+            print(message_uuids)  
+            print("კავშირის დახურვა", datetime.datetime.now())
+            connection.shutdown(socket.SHUT_RDWR)
             connection.close()
+            break
+    print("იხურება: wait_for_server_response")
 
 # ფუნქციის საშუალებით ies_monitoring_server-ს შეგვიძლია გავუგზავნოთ შეტყობინება
 def send_message(message_type, text):
     # დავუკავშირდეთ სერვერს
     connect_to_ies_monitoring_server()
+
     # შეტყობინების უნიკალური id
     message_id = str(uuid.uuid4())
     # მესიჯის გაგზავნის დრო
@@ -64,18 +79,19 @@ def send_message(message_type, text):
     connection.send(dictionary_to_bytes(message_data))
     # გავწყვიტოთ კავშირი სერვერთან
     # connection.close()
-    if wait_for_server_response_thread is None or not wait_for_server_response_thread.is_alive():
-        start_wait_for_server_response_thread()
+    start_wait_for_server_response_thread(connection, message_id)
+   
 
 # thread-ის გამოყენებით შეტყობინების გაგზავნა.
 def send_message_using_thread(message_type, text):
     send_message_thread = threading.Thread(target = send_message, args = (message_type, text))
     send_message_thread.start()
 
-def start_wait_for_server_response_thread():
-    wait_for_server_response_thread = threading.Thread(target = wait_for_server_response, name = 'wait_for_server_response_thread')
+def start_wait_for_server_response_thread(connection, message_id):
+    wait_for_server_response_thread = threading.Thread(target = wait_for_server_response, args = (connection, message_id))
     wait_for_server_response_thread.start()
+    print("გაეშვა start_wait_for_server_response_thread")
 
 if __name__ == "__main__":
-    send_message("ა", "ბ")
+    send_message("blockkk", "ბ")
     send_message("aaaa", "სერვერი დაიწვა")
