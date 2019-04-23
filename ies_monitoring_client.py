c376e8df1c0a558e0397be6df9ce0ddf2a7cefe8
Irakli Gunia <irakli.gunia@gmail.com>
2019-04-23 19:15:47 +0400

    connect_to_ies_monitoring_server აბრუნებს connection ობიექტს და აღარ არის გლობალური. send_message ფუნქციას დაემატა resend_try_number, resend_delay პარამეტრები

--- ies_monitoring_client.py
+++ ies_monitoring_client.py
@@ -23,66 +23,61 @@ def string_to_datetime(date_time_str):
 
 # ფუნქცია ქმნის სოკეტს და უკავშირდება ies_monitoring_server-ს
 def connect_to_ies_monitoring_server():
-    print("კავშირის დაწყება")
-    # გლობალურად ავღწეროთ connection ცვლადი
-    global connection
     # შევქმნათ სოკეტი
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # დავუკავშირდეთ ies_monitoring_server-ს
     connection.connect((server_ip, server_port))
-    print("დაკავშირდა")
+    return connection
+
+def connection_close(connection):
+    connection.shutdown(socket.SHUT_RDWR)
+    connection.close()
 
 # dictionary გადაყავს bytes ტიპში
 def dictionary_to_bytes(dictionary_message):
     return bytes(json.dumps(dictionary_message), 'utf-8')
 
-def wait_for_server_response(connection, message_id, using_threading):
-    print("=====================wait_for_server_response========================")
+def wait_for_server_response(connection, message_id, resend_try_number, resend_delay, using_threading):
     # სანამ sent_messages არ არის ცარიელი
     # სანამ სერვერიდან ველოდებით შეტყობინებას მესიჯის მიღების დასტურად
-    print(id(connection))
-    # print(type(connection))
     while True:
-        # print("++++++++++++++++++++++++++++++++++++++++++++++++++")
-        # print(sent_messages)
-
-        # print(connection)
         time.sleep(0.2)
         received_message_bytes = connection.recv(buffer_size)
         received_message_id = received_message_bytes.decode("utf-8")
         if received_message_id == "":
             message_response_delay = datetime.datetime.now() - string_to_datetime(sent_messages[message_id]["sent_message_datetime"])
-            print(type(message_response_delay))
-            if message_response_delay > datetime.timedelta(seconds=3):
+            print(message_response_delay)
+            if message_response_delay > datetime.timedelta(seconds=resend_delay):
+                print("wait_for_server_response|" , sent_messages[message_id]["sent_message_count"], "|")
+                print(type(sent_messages[message_id]["sent_message_count"]))
                 sent_message_count = sent_messages[message_id]["sent_message_count"] + 1
-                connection.shutdown(socket.SHUT_RDWR)
-                connection.close()
+                if sent_message_count > resend_try_number:
+                    connection_close(connection)
+                    print("SMS გაგზავნა")
+                    break
+                connection_close(connection)
                 print("resend count = ",sent_message_count)
-                resend_message(sent_messages[message_id], sent_message_count, using_threading)
+                resend_message(sent_messages[message_id], resend_try_number, resend_delay, sent_message_count, using_threading)
                 break
-            #print("მესიჯი არ მოსულა")
-            #print(sent_messages[message_id]["sent_message_datetime"])
-            #print(datetime.datetime.now() - string_to_datetime(sent_messages[message_id]["sent_message_datetime"]))
             continue
         if received_message_id in sent_messages:
             print(":::::::" + received_message_id)
-            print(sent_messages)
-            del sent_messages[received_message_id]
-            print("-----------")
-            print(sent_messages)  
+            del sent_messages[received_message_id] 
             print("კავშირის დახურვა", datetime.datetime.now())
             connection.shutdown(socket.SHUT_RDWR)
             connection.close()
             break
-    print("იხურება: wait_for_server_response")
 
 # ფუნქციის საშუალებით ies_monitoring_server-ს შეგვიძლია გავუგზავნოთ შეტყობინება
-def send_message_task(message_type, text, sent_message_count, using_threading):
+def send_message_task(message_id, message_type, text, resend_try_number, resend_delay, sent_message_count, using_threading):
+    print("send_message_task|" ,sent_message_count, "|")
+    print("send_message_task=", type(sent_message_count))
     # დავუკავშირდეთ სერვერს
-    connect_to_ies_monitoring_server()
+    connection = connect_to_ies_monitoring_server()
 
-    # შეტყობინების უნიკალური id
-    message_id = str(uuid.uuid4())
+    if not message_id:
+        # შეტყობინების უნიკალური id
+        message_id = str(uuid.uuid4())
     # მესიჯის გაგზავნის დრო
     sent_message_datetime = str(datetime.datetime.now())
     # შევქმნათ message_data dictionary რასაც ვუგზავნით სერვერს
@@ -92,38 +87,45 @@ def send_message_task(message_type, text, sent_message_count, using_threading):
                     "sent_message_datetime": sent_message_datetime,
                     "sent_message_count" : sent_message_count}
 
+
     # sent_messages dictionary -ში ჩავამატოთ მიმდინარე მესიჯის უნიკალური Id-ი და დრო
     sent_messages[message_id] = message_data
     # გავაგზავნოთ შეტყობინება
-    print(dictionary_to_bytes(message_data))
     connection.send(dictionary_to_bytes(message_data))
     # გავწყვიტოთ კავშირი სერვერთან
     # connection.close()
     if using_threading:
-        start_wait_for_server_response_thread(connection, message_id)
+        start_wait_for_server_response_thread(connection, message_id, resend_try_number, resend_delay)
     else:
-        wait_for_server_response(connection, message_id, False)
+        wait_for_server_response(connection, message_id, resend_try_number, resend_delay, False)
 
 # thread-ის გამოყენებით შეტყობინების გაგზავნა.
-def send_message_using_threading(message_type, text, sent_message_count):
-    send_message_thread = threading.Thread(target = send_message_task, args = (message_type, text, sent_message_count, True))
+def send_message_using_threading(message_id, message_type, text, resend_try_number, resend_delay, sent_message_count):
+    print("send_message_using_threading|" ,sent_message_count, "|")
+    print("send_message_using_threading=",type(sent_message_count))
+    send_message_thread = threading.Thread(target = send_message_task, args = (message_id, message_type, text, resend_try_number, resend_delay, sent_message_count, True))
     send_message_thread.start()
 
-def send_message(message_type, text, sent_message_count=1, using_threading=True):
+def send_message(message_type, text, resend_try_number = 3, resend_delay = 3,  sent_message_count=1, using_threading=True, message_id = False):
+    print("send_message|" ,sent_message_count, "|"," =")
+    sent_message_count = int(sent_message_count)
+    print("send_message=",type(sent_message_count))
     if using_threading:
-        send_message_using_threading(message_type, text, sent_message_count)
+        send_message_using_threading(message_id, message_type, text, resend_try_number, resend_delay, sent_message_count)
     else:
-        send_message_task(message_type, text, sent_message_count, using_threading=False)
+        send_message_task(message_id, message_type, text, resend_try_number, resend_delay, sent_message_count, using_threading=False)
 
-def resend_message(message_data, sent_message_count, using_threading):
+def resend_message(message_data, resend_try_number, resend_delay, sent_message_count, using_threading):
+    message_id = message_data["message_id"]
     message_type = message_data["message_type"]
     text = message_data["text"]
-    send_message(message_type, text, sent_message_count, using_threading)
+    print("resend_message|" ,sent_message_count, "|")
+    print("resend_message=",type(sent_message_count))
+    send_message(message_type, text, resend_try_number, resend_delay, sent_message_count, using_threading, message_id = False)
 
-def start_wait_for_server_response_thread(connection, message_id):
-    wait_for_server_response_thread = threading.Thread(target = wait_for_server_response, args = (connection, message_id, True))
+def start_wait_for_server_response_thread(connection, message_id, resend_try_number, resend_delay,):
+    wait_for_server_response_thread = threading.Thread(target = wait_for_server_response, args = (connection, message_id, resend_try_number, resend_delay, True))
     wait_for_server_response_thread.start()
-    print("გაეშვა start_wait_for_server_response_thread")
 
 if __name__ == "__main__":
     send_message("blockkk", "ბ", using_threading=False)
